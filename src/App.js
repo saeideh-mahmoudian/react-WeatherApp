d6485c4dc1985ddc49bdb844dce9f71da2423cfe
saeideh-mahmoudian <s.mahmoudian@gmail.com>
2023-07-06 19:47:45 +0330

    add my GitHub access as a footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Saeideh</h1>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href='https://www.shecodes.io/graduates/68983-saeideh-mahmoudian' target='_blank' >Saeideh Mahmoudian</a> and is <a href='https://github.com/saeideh-mahmoudian/react-WeatherApp' target="_blank">open-sourced on GitHub</a> 
+      </footer>
     </div>
   );
 }
