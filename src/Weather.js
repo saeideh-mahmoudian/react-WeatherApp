40d3af28863af1ff07cdc1cf5f4f5feded01e5f8
saeideh-mahmoudian <s.mahmoudian@gmail.com>
2023-07-07 13:30:44 +0330

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,11 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +27,19 @@ export default function Weather() {
         <li>Friday 09:36</li>
         <li>Broken Clouds</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly cloudy"
-          />
-          23°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly cloudy"
+              className="float-left"
+            />
+            <span className="float-left">
+              <span className="temperature">23</span>
+              <span className="unit">°C</span>
+            </span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
