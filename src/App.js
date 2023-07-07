685f414422ad5cb3719ee1d22f3aec92cbc0abb6
saeideh-mahmoudian <s.mahmoudian@gmail.com>
2023-07-07 10:00:47 +0330

    create Weather component and insert HTML codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href='https://www.shecodes.io/graduates/68983-saeideh-mahmoudian' target='_blank' >Saeideh Mahmoudian</a> and is <a href='https://github.com/saeideh-mahmoudian/react-WeatherApp' target="_blank">open-sourced on GitHub</a> 
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/68983-saeideh-mahmoudian"
+            target="_blank"
+          >
+            Saeideh Mahmoudian
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/saeideh-mahmoudian/react-WeatherApp"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
