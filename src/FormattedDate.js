aad126d8cfc55935a67a58c8ff6bf323640127f9
saeideh-mahmoudian <s.mahmoudian@gmail.com>
2023-07-15 00:29:57 +0330

    added unit conversion and fixed FormattedDate

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-  const days = [
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -10,9 +10,15 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday",
   ];
-  const day = days[props.date.getDay()];
-  const hours = props.date.getHours();
-  const minutes = props.date.getMinutes();
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return (
     <div className="FormattedDate">
       {day} {hours}:{minutes}
